ebc628119b45af8341e849d3c81c2fbddb115737
Weddy Nkatha <115021897+WeddyNkatha265@users.noreply.github.com>
2024-08-13 10:49:49 +0300

    Update QuestionItem.js

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-function QuestionItem({ question, onDeleteQuestion, onUpdateCorrectAnswer }) {
+function QuestionItem({ question, onDelete, onChange}) {
+
+
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,29 +11,40 @@ function QuestionItem({ question, onDeleteQuestion, onUpdateCorrectAnswer }) {
     </option>
   ));
 
-  function handleDelete() {
-    const id = question.id;
-    fetch(`http://localhost:4000/questions/${id}`, {
-      method: "DELETE",
-    })
-      .then(() => {
-       
-        onDeleteQuestion(id);
-      })
-  
+ function handleDelete(){
+ 
+  fetch(`http://localhost:4000/questions/${id}`, {
+    method: "DELETE",
+  })
+    .then((r) => r.json())
+    .then(() => onDelete(question));
+
   }
 
-  function handleSelectChange(event) {
-    const newCorrectIndex = parseInt(event.target.value);
+  
+  function handleChange(event){
 
-    fetch(`http://localhost:4000/questions/${id}`, {
-      method: "PATCH",
-      headers: {
+    const newCorrectIndex= parseInt(event.target.value)
+
+
+     fetch(`http://localhost:4000/questions/${id}`, {
+       method: "PATCH",
+       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ correctIndex: newCorrectIndex }),
-    
+       body: JSON.stringify({
+         "correctIndex": newCorrectIndex
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedQuestion) => {
+        
+        onChange(updatedQuestion)
+
+        console.log(updatedQuestion)
+      
       });
+   
   }
 
   return (
@@ -40,9 +53,7 @@ function QuestionItem({ question, onDeleteQuestion, onUpdateCorrectAnswer }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select value={correctIndex} onChange={handleSelectChange}>
-          {options}
-        </select>
+        <select onChange={handleChange} defaultValue={correctIndex}>{options}</select>
       </label>
       <button onClick={handleDelete}>Delete Question</button>
     </li>
